e2777f5214761b5e470765b6e3847c04d0704359
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2016-04-29 23:02:24 +0200

    assignment 4

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace watermarking {
+	class MainClass {
+		static void Main (string[] args) {
+			Console.Write("Enter watermark strength: ");
+			int alpha = getValue ();
+
+			Console.Write ("Enter a seed value: ");
+			int seed = getValue ();
+
+			Console.Write ("Enter filename: ");
+			string filename = Console.ReadLine ();
+
+			Console.Write ("Enter \"mark\" to watermark image, or \"check\" to check for watermark: ");
+			string option = Console.ReadLine ();
+
+			Watermarker wm = new Watermarker (filename, seed, alpha);
+
+			if (option == "mark") {
+				wm.watermark ();
+			} else if (option == "check") {
+				Console.WriteLine("File is watermarked: {0}", wm.isWatermarked ());
+			}
+		}
+
+		public static int getValue(){
+			int value;
+			bool success = int.TryParse(Console.ReadLine(), out value);
+
+			while (!success) {
+				Console.Write ("Enter a valid Value: ");
+				success = int.TryParse(Console.ReadLine(), out value);
+			}
+
+			return value;
+		}
+	}
+}
---
+++ Watermarker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+
+namespace watermarking
+{
+	public class Watermarker
+	{
+		public Bitmap img;
+		private string filename;
+		private int seed;
+		private int alpha;
+
+		public Watermarker (string filename, int seed, int alpha)
+		{
+			this.filename = filename;
+			this.img = new Bitmap (Image.FromFile(filename));
+			this.seed = seed;
+			this.alpha = alpha;
+		}
+
+		public void watermark(){
+			Random rand = new Random (seed);
+			int g;
+			int r;
+			int b;
+			for (int x = 0; x < img.Width; x++) {
+				for (int y = 0; y < img.Height; y++) {
+					r = img.GetPixel (x, y).R;
+					g = img.GetPixel (x, y).G + (rand.Next (-1,2) * alpha);
+					if (g > 255) {
+						g = 255;
+					} else if (g < 0) {
+						g = 0;
+					}
+					b = img.GetPixel (x, y).B;
+
+					img.SetPixel(x,y,Color.FromArgb(r,g,b));
+				}
+			}
+
+			img.Save (filename.Substring(0,filename.Length-4)+"_marked.png", System.Drawing.Imaging.ImageFormat.Png);
+		}
+
+		public bool isWatermarked(){
+			Random rand = new Random (seed);
+			double sum = 0;
+
+			for (int x = 0; x < img.Width; x++) {
+				for (int y = 0; y < img.Height; y++) {
+					sum += img.GetPixel (x, y).G * (rand.Next (-1, 2) * alpha);
+				}
+			}
+
+			double correlationskoeffizient = sum / (img.Height * img.Width);
+
+			if(correlationskoeffizient > 12)
+				return true;
+			return false;
+		}
+	}
+}
+
